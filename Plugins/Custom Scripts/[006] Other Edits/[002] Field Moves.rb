9d9d5928f4a7dcf7701fe84a1d33258cdae1ec2d
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2025-08-06 19:27:11 -0400

    I hope the Surf & Fly edits work

---
+++ Plugins/Custom Scripts/[006] Other Edits/[002] Field Moves.rb
@@ -0,0 +1,38 @@
+# Flying
+def pbCanFly?(pkmn = nil, show_messages = false)
+  return false if !pbCheckHiddenMoveBadge(Settings::BADGE_FOR_FLY, show_messages)
+  return false if !$DEBUG && !pkmn && !$player.get_pokemon_with_move(:FLY)
+  return true if pbCheckHiddenMoveBadge(Settings::BADGE_FOR_FLY, show_messages)
+  if !$game_player.can_map_transfer_with_follower?
+    pbMessage(_INTL("It can't be used when you have someone with you.")) if show_messages
+    return false
+  end
+  if !$game_map.metadata&.outdoor_map
+    pbMessage(_INTL("You can't use that here.")) if show_messages
+    return false
+  end
+  return true
+end
+
+
+def pbSurf
+  return false if !$game_player.can_ride_vehicle_with_follower?
+  move = :SURF
+  movefinder = $player.get_pokemon_with_move(move)
+  if !pbCheckHiddenMoveBadge(Settings::BADGE_FOR_SURF, false) || (!$DEBUG && !movefinder)
+    return false
+  elsif pbCheckHiddenMoveBadge(Settings::BADGE_FOR_SURF, true) || (!$DEBUG && !movefinder)
+    return true
+  end
+  if pbConfirmMessage(_INTL("The water is a deep blue color... Would you like to use Surf on it?"))
+    speciesname = (movefinder) ? movefinder.name : $player.name
+    pbMessage(_INTL("{1} used {2}!", speciesname, GameData::Move.get(move).name))
+    pbCancelVehicles
+    pbHiddenMoveAnimation(movefinder)
+    surfbgm = GameData::Metadata.get.surf_BGM
+    pbCueBGM(surfbgm, 0.5) if surfbgm
+    pbStartSurfing
+    return true
+  end
+  return false
+end
