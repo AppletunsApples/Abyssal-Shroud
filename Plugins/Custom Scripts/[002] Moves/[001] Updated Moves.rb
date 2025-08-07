b5e198384d702326954b0a901e4b0f3e67446464
Sethphyr62 <nicolasfv69@gmail.com>
2025-08-07 12:39:56 -0400

    Update [001] Updated Moves.rb
    tweakin

--- Plugins/Custom Scripts/[002] Moves/[001] Updated Moves.rb
+++ Plugins/Custom Scripts/[002] Moves/[001] Updated Moves.rb
@@ -1,7 +1,7 @@
 # Will O Wisp
 class Battle::Move::BurnTargetAlwaysHitsInSun < Battle::Move::BurnTarget
   def pbBaseAccuracy(user, target)
-    return 0 if [:Sun, :Harsh Sun].include?(target.effectiveWeather)
+    return 0 if [:Sun,:Harsh Sun].include?(target.effectiveWeather)
     return super
   end
 end
