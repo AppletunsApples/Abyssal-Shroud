d25726af5f203f7e95f204d8568ac0287e3f844e
Oliver's Apples <95107150+AppletunsApples@users.noreply.github.com>
2026-06-04 09:53:44 -0400

    Setting up the new evolutions of V2

--- Plugins/Custom Scripts/[006] Other Edits/[002] Field Moves.rb
+++ Plugins/Custom Scripts/[006] Other Edits/[002] Field Moves.rb
@@ -1,5 +1,6 @@
 def pbSurf
-  return false if !$game_player.can_ride_vehicle_with_follower?
+  return false if !$game_player.can_ride_vehicle_with_follower?\
+  return true if $DEBUG
   return false if !pbCheckHiddenMoveBadge(Settings::BADGE_FOR_SURF, true)
   if pbConfirmMessage(_INTL("The water is a deep blue color... Would you like to use Surf on it?"))
     pbMessage(_INTL("\\PN started surfing!"))
@@ -15,6 +16,7 @@ end
 
 def pbCanFly?(pkmn = nil, show_messages = false)
   return false if !pbCheckHiddenMoveBadge(Settings::BADGE_FOR_FLY, show_messages)
+  return true if $DEBUG
   if !$game_player.can_map_transfer_with_follower?
     pbMessage(_INTL("It can't be used when you have someone with you.")) if show_messages
     return false
