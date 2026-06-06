62c3196fb47e1335e16e72ad121746c2f861d8e4
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2026-06-05 22:14:00 -0400

    Update [000] New Abilities.rb

--- Plugins/Custom Scripts/[003] Abilities/[000] New Abilities.rb
+++ Plugins/Custom Scripts/[003] Abilities/[000] New Abilities.rb
@@ -1,50 +1,9 @@
-# Diamond Dust
-Battle::AbilityEffects::OnSwitchIn.add(:DIAMONDDUST,
-proc { |ability, battler, battle, switch_in|
-      battle.pbStartWeatherAbility(:Snow, battler)
-      next if battler.pbOwnSide.effects[PBEffects::AuroraVeil] > 0
-      battler.pbOwnSide.effects[PBEffects::AuroraVeil] = 5
-      battler.pbOwnSide.effects[PBEffects::AuroraVeil] = 8 if battler.hasActiveItem?(:LIGHTCLAY)
-    battle.pbDisplay(_INTL("{1} made {2} stronger against physical and special moves!",
-                            battler.name, battler.pbTeam(true)))
-    }
-)
-
 # In Spirit
 Battle::AbilityEffects::DamageCalcFromUser.add(:INSPIRIT,
   proc { |ability, user, target, move, mults, power, type|
     mults[:attack_multiplier] *= 1.5 if type == :GHOST
   }
 )
-
-# Judgement Day
-Battle::AbilityEffects::OnSwitchIn.add(:JUDGEMENTDAY, 
-proc { |ability, battler, battle|
-  next if battler.fainted?
-  battle.eachOtherSideBattler(battler.index) do |foe|
-    next if foe.fainted?
-    next if foe.effects[PBEffects::PerishSong] > 0
-    foe.effects[PBEffects::PerishSong] = 3
-    foe.effects[PBEffects::PerishSongUser] = battler.index
-    battle.pbShowAbilitySplash(battler)
-    battle.pbDisplay(_INTL("{1} was caught in the Death Song!", foe.pbThis))
-    battle.pbHideAbilitySplash(battler)
-  end
-})
-
-Battle::AbilityEffects::EndOfRoundEffect.add(:JUDGEMENTDAY,
-proc { |ability, battler, battle|
-  next if battler.fainted?
-  battle.eachOtherSideBattler(battler.index) do |foe|
-    next if foe.fainted?
-    next if foe.effects[PBEffects::PerishSong] > 0
-    foe.effects[PBEffects::PerishSong] = 3
-    foe.effects[PBEffects::PerishSongUser] = battler.index
-    battle.pbShowAbilitySplash(battler)
-    battle.pbDisplay(_INTL("{1} was caught in the Death Song!", foe.pbThis))
-    battle.pbHideAbilitySplash(battler)
-  end
-})
 
 # Valor
 Battle::AbilityEffects::OnEndOfUsingMove.add(:VALOR,
